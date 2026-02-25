6226ad20cc5139daf4aa327105e8e532ff21b966
happy inside <happy@gmail.com>
2026-02-25 22:44:04 +0300

    client

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcServer, &server{})
+}
