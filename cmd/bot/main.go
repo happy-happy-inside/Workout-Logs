b923d3f9687c7e8328fd8503aff6a4e99a2cbeeb
happy inside <happy@gmail.com>
2026-02-26 19:37:54 +0300

    add bot logic

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,60 +1,30 @@
 package main
 
 import (
-	"context"
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-type Massage struct {
-}
-
-func Connect() error {
-
-}
-
-func ParsMassage() *Massage {
-
-}
-
-func ChoiseFunc() {
-
-}
-
 func main() {
-	bot, err := tgbotapi.NewBotAPI("TOKEN")
+	token := os.Getenv("TELEGRAM_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	proto.AddRes
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(tgbotapi.UpdateConfig{Timeout: 60})
+	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
 
-		// вызываем backend
-		resp, err := client.Hello(
-			context.Background(),
-			&pb.HelloRequest{
-				Name: update.Message.From.UserName,
-			},
-		)
-
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		msg := tgbotapi.NewMessage(
-			update.Message.Chat.ID,
-			resp.Message,
-		)
-
-		bot.Send(msg)
+		handleMessage(bot, update.Message)
 	}
 }
