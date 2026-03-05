baebd6d3e17274a714029692b677c7076df9d2f8
happy inside <happy@gmail.com>
2026-03-05 10:20:04 +0300

    finish redis client

--- WorkoutServer/internal/redis/redis.go
+++ WorkoutServer/internal/redis/redis.go
@@ -4,26 +4,38 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"sync"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 type RedisClient struct {
+	defaultTTL     time.Duration
 	defaultTimeout time.Duration
 	rdb            *redis.Client
-	mu             sync.RWMutex
 }
 
-func NewRedisClient(defaultTimeout time.Duration, redisAddr string) (*RedisClient, error) {
+func (r *RedisClient) checkTimeout(ctx context.Context) (context.Context, context.CancelFunc) {
+	if ctx == nil {
+		return context.WithTimeout(context.Background(), r.defaultTimeout)
+	}
+
+	if _, ok := ctx.Deadline(); ok {
+		return ctx, func() {}
+	}
+
+	return context.WithTimeout(ctx, r.defaultTimeout)
+}
+
+func NewRedisClient(defaultTimeout time.Duration, defaultTTL time.Duration, redisAddr string) (*RedisClient, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr: redisAddr,
 	})
 
 	redisClient := &RedisClient{
-			defaultTimeout: defaultTimeout,
+		defaultTimeout: defaultTimeout,
 		rdb:            client,
+		defaultTTL:     defaultTTL,
 	}
 
 	if err := redisClient.Ping(); err != nil {
@@ -34,25 +46,45 @@ func NewRedisClient(defaultTimeout time.Duration, redisAddr string) (*RedisClien
 }
 
 func (r *RedisClient) Ping() error {
-	
+
 	for range 5 {
 		ctx, cancel := context.WithTimeout(context.Background(), r.defaultTimeout)
-		defer cancel()
 
 		err := r.rdb.Ping(ctx).Err()
+		cancel()
+
 		if nil == err {
 			return nil
 		}
+
 		log.Print(err)
 	}
-	
+
 	return fmt.Errorf("Redis not reachebl")
 }
 
-func (r *RedisClient) Set(ctx context.Context, key string, val string) error {
+func (r *RedisClient) Set(ctx context.Context, key string, val string, ttl time.Duration) error {
+	if ttl == 0 {
+		ttl = r.defaultTTL
+	}
 
+	ctx, cancel := r.checkTimeout(ctx)
+	defer cancel()
+
+	if err := r.rdb.Set(ctx, key, val, ttl).Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *RedisClient) Get(ctx context.Context, key string) (string, error) {
+	ctx, cancel := r.checkTimeout(ctx)
+	defer cancel()
 
+	if res := r.rdb.Get(ctx, key).Val(); res != "" {
+		return res, nil
+	}
+
+	return "", fmt.Errorf("Key not set")
 }
